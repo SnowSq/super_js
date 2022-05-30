33f994e5ce7521fad3ab3c9491d55e583ad2c24b
SnowSq <92370031+SnowSq@users.noreply.github.com>
2022-05-30 20:46:41 +0300

    hw 2
    Сделала задание без звездочки (калькулятор для товаров, метод reduce)

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
+  { title: 'Shirt', price: 150 },
+  { title: 'Socks', price: 50 },
+  { title: 'Jacket', price: 350 },
+  { title: 'Shoes', price: 250 }
+]
 
-    const renderGoodsItem = ({title = ' ', price = ' '}) => 
-    `
-      <div class="goods-item">
-        <h3>${title}</h3>
-        <p>${price}</p>
-      </div>
-    `;
-  
-  const renderGoodsList = (list = []) => {
-    let goodsList = list.map(item => renderGoodsItem(item)).join (' ');
-    document.querySelector('.goods-list').innerHTML = goodsList;
+class GoodsItem {
+  constructor ({ title, price }) {
+    this.title = title
+    this.price = price
   }
-  
-  renderGoodsList(goods);
-  
+  render () {
+    return `
+    <div class="goods-item">
+      <h3>${this.title}</h3>
+      <p>${this.price}</p>
+    </div>
+  `
+  }
+}
+class GoodsList {
+  items = []
+  fetchGoods () {
+    this.items = goods
+  }
+  countGoodsList () {
+    return this.items.reduce((prev, { price }) => {
+      return prev + price
+    }, 0)
+  }
+  render () {
+    const goods = this.items
+      .map(item => {
+        const goodItem = new GoodsItem(item)
+        return goodItem.render()
+      })
+      .join('')
 
+    document.querySelector('.goods-list').innerHTML = goods
+  }
+}
 
-  
-  
-  
+const goodsList = new GoodsList()
+goodsList.fetchGoods()
+goodsList.render()
